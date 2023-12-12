c459497767aa35c14b84f0bdd9449c4a823a0d92
Jianqiang LI <1155157143@link.cuhk.edu.hk>
2023-12-12 14:22:03 +0800

    Add files via upload

--- MyDedup.java
+++ MyDedup.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 import static java.lang.String.*;
-
+import java.nio.file.*;
 
 public class MyDedup {
     public static void main(String[] args) throws Exception {
@@ -10,12 +10,12 @@ public class MyDedup {
             helpInfo();
             System.exit(1);
         }
-        
+
         String operation = args[0];
-        try{
+        try {
             switch (operation) {
                 case "upload":
-                    if (args.length != 6){
+                    if (args.length != 6) {
                         helpInfo();
                         System.exit(1);
                     }
@@ -24,15 +24,19 @@ public class MyDedup {
                     int maxChunk = Integer.parseInt(args[3]);
                     int base = Integer.parseInt(args[4]);
                     String fileToUpload = args[5];
-                    if (!fileExistence(fileToUpload)){
+                    if (!fileExistence(fileToUpload)) {
                         System.err.println("[ERROR] The file to upload does does not exist!!!");
                         System.exit(1);
+                    }
+                    File directory = new File("data");
+                    if (!directory.exists()) {
+                        directory.mkdir();
                     }
                     MyUpload myUpload = new MyUpload(minChunk, avgChunk, maxChunk, base, fileToUpload);
                     myUpload.upload();
                     break;
                 case "download":
-                    if (args.length != 3){
+                    if (args.length != 3) {
                         helpInfo();
                         System.exit(1);
                     }
@@ -44,19 +48,19 @@ public class MyDedup {
                 default:
                     break;
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void helpInfo() {
         System.out.println("Sample Input/Output Format: ");
-        System.out.println("    Upload: java MyDedup upload <min_chunk> <avg_chunk> <max_chunk> <base> <file_to_upload>");
+        System.out
+                .println("    Upload: java MyDedup upload <min_chunk> <avg_chunk> <max_chunk> <base> <file_to_upload>");
         System.out.println("    Download: java MyDedup download <file_to_download> <local_file_name>");
     }
 
-    public static boolean fileExistence(String filename){
+    public static boolean fileExistence(String filename) {
         File file = new File(filename);
         boolean existence = file.exists();
         return existence;
