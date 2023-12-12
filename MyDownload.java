e0f998ef6c20515edce301eeac0d8388141324d2
Jianqiang LI <1155157143@link.cuhk.edu.hk>
2023-12-12 14:40:45 +0800

    support local_file_name contains path

--- MyDownload.java
+++ MyDownload.java
@@ -12,27 +12,26 @@ public class MyDownload {
     private String localFileName;
     private MetaData metadata;
 
-
-    public MyDownload(String fileToDownload, String localFileName) throws IOException{
+    public MyDownload(String fileToDownload, String localFileName) throws IOException {
         this.fileToDownload = fileToDownload;
         this.localFileName = localFileName;
         this.metadata = new MetaData();
-        File file = new File("mydedup.index"); 
-        if (!file.exists()){
+        File file = new File("mydedup.index");
+        if (!file.exists()) {
             System.err.println("[ERROR] Cannot download an nonexistent file!!!");
             System.exit(1);
-        } 
+        }
         this.metadata.read("mydedup.index");
     }
 
-    public void download() throws FileNotFoundException, IOException, ClassNotFoundException{
+    public void download() throws FileNotFoundException, IOException, ClassNotFoundException {
         ObjectInputStream input;
         String containerPath;
         byte[] myContent;
         String[] tmp = this.fileToDownload.split("/");
-        String fileName = tmp[tmp.length -1];
-        
-        if (this.metadata.getFileRecipe().containsKey(fileName)){
+        String fileName = tmp[tmp.length - 1];
+
+        if (this.metadata.getFileRecipe().containsKey(fileName)) {
             FileRecipe fileRecipe = this.metadata.getFileRecipe().get(fileName);
             HashMap<String, Chunk> mapping = this.metadata.getMapping();
             ArrayList<String> fingerprintList = fileRecipe.getFingerprintList();
@@ -43,19 +42,19 @@ public class MyDownload {
             int chunkLength;
             int contentStartingOffset = 0;
             Set<String> containerSet = new HashSet<>();
-            for (int i = 0; i < fingerprintList.size(); i++){
+            for (int i = 0; i < fingerprintList.size(); i++) {
                 currentChunk = mapping.get(fingerprintList.get(i));
                 int tmpLength = currentChunk.getChunkLength();
                 int tmpOffset = contentStartingOffset;
-                if (!containerSet.contains(currentChunk.getContainerIndex())){
+                if (!containerSet.contains(currentChunk.getContainerIndex())) {
                     containerSet.add(Integer.toString(currentChunk.getContainerIndex()));
                     containerPath = "data/container_" + Integer.toString(currentChunk.getContainerIndex());
                     input = new ObjectInputStream(new FileInputStream(containerPath));
                     container = (byte[]) input.readObject();
                     int tmpContainerIndex = currentChunk.getContainerIndex();
-                    for (int j = i; j < fingerprintList.size(); j++){
+                    for (int j = i; j < fingerprintList.size(); j++) {
                         currentChunk = mapping.get(fingerprintList.get(j));
-                        if (currentChunk.getContainerIndex() == tmpContainerIndex){
+                        if (currentChunk.getContainerIndex() == tmpContainerIndex) {
                             containerOffset = currentChunk.getOffset();
                             chunkLength = currentChunk.getChunkLength();
                             System.arraycopy(container, containerOffset, myContent, tmpOffset, chunkLength);
@@ -65,14 +64,21 @@ public class MyDownload {
                 }
                 contentStartingOffset += tmpLength;
             }
-            try (FileOutputStream fos = new FileOutputStream(this.localFileName)) {
-                fos.write(myContent); 
+            File file = new File(this.localFileName);
+            String[] tmp2 = this.localFileName.split("/");
+            for (int i = 0; i < tmp2.length - 1; i++) {
+                File directory = new File(tmp2[i]);
+                if (!directory.exists()) {
+                    directory.mkdir();
+                }
+            }
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                fos.write(myContent);
                 System.out.println("[INFO] " + this.fileToDownload + " has been downloaded successfully!");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else{
+        } else {
             System.err.println("[ERROR] Cannot download an nonexistent file!!!");
             System.exit(1);
         }
